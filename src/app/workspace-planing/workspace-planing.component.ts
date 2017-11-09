c016aec8eee7522babe83a1e0df36d86f4e98fb2
Jona Wössner <jonawoessner@gmail.com>
2017-11-09 18:42:43 +0100

    get production que and in production from datapase

--- src/app/workspace-planing/workspace-planing.component.ts
+++ src/app/workspace-planing/workspace-planing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-workspace-planing',
@@ -7,69 +9,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkspacePlaningComponent implements OnInit {
 
-  workspaces = {
-    workspace: [1,1,1,2,2,2,3,3,3,4,4,4,6,6,6,6,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,11,11,11,11,11,11,12,12,12,12,12,12,13,13,13,13,13,13,14,15,15],
-    id: [49,54,29,50,55,30,51,56,31,1,2,3,16,18,19,20,10,11,12,13,14,15,18,19,20,26,10,11,12,13,14,15,18,19,20,10,11,12,13,14,15,18,19,20,4,5,6,7,8,9,4,5,6,7,8,9,10,11,12,13,14,15,10,11,12,13,14,15,16,17,26],
+  currentPeriod = 3;
+
+  worktime = {
+    workplace: [1,1,1,2,2,2,3,3,3,4,4,4,6,6,6,6,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,11,11,11,11,11,11,12,12,12,12,12,12,13,13,13,13,13,13,14,15,15],
+    item: [49,54,29,50,55,30,51,56,31,1,2,3,16,18,19,20,10,11,12,13,14,15,18,19,20,26,10,11,12,13,14,15,18,19,20,10,11,12,13,14,15,18,19,20,4,5,6,7,8,9,4,5,6,7,8,9,10,11,12,13,14,15,10,11,12,13,14,15,16,17,26],
     time: [6,6,6,5,5,5,5,6,6,6,7,7,2,3,3,3,2,2,2,2,2,2,2,2,2,2,1,2,2,1,2,2,3,3,3,3,3,3,3,3,3,2,2,2,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,3,3,3]
   };
 
+  setuptime = {
+    workplace: [1,1,1,2,2,2,3,3,3,4,4,4,6,6,6,6,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,11,11,11,11,11,11,12,12,12,12,12,12,13,13,13,13,13,13,14,15,15],
+    item: [49,54,29,50,55,30,51,56,31,1,2,3,16,18,19,20,10,11,12,13,14,15,18,19,20,26,10,11,12,13,14,15,18,19,20,10,11,12,13,14,15,18,19,20,4,5,6,7,8,9,4,5,6,7,8,9,10,11,12,13,14,15,10,11,12,13,14,15,16,17,26],
+    time: [20,20,20,30,30,30,20,20,20,30,20,30,15,15,15,15,20,20,20,20,20,20,20,20,20,30,15,15,15,15,15,15,20,25,20,15,15,15,15,15,15,15,20,15,20,20,20,20,20,20,10,10,20,20,20,20,0,0,0,0,0,0,0,0,0,0,0,0,0,15,15]
+  };
+
   productionPlan = {
-    id: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,26,49,54,29,50,55,30,51,56,31],
+    item: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,26,49,54,29,50,55,30,51,56,31],
     amount: [150,100,100,130,100,100,130,100,100,130,100,100,130,100,100,680,680,130,100,100,330,130,100,100,280,200,200,180,100,200]
   };
 
-  productionQue = {
-    workspace: [7,8,9],
-    time:[300,200,100]
-  };
+  productionQue = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-  inProduction = {
-    workspace: [7],
-    time: [50]
-  };
+  inProduction = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-  workspacePlan = {
+  workplacePlan = {
     totalTime: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     shift: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     overtime: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   };
 
-  constructor() { }
+
+
+  previousPeriodData: Observable<any>;
+
+  constructor(db: AngularFireDatabase) {
+
+    this.previousPeriodData = db.object('periods/' + (this.currentPeriod-1)).valueChanges();
+  }
 
   ngOnInit() {
-
-    for (let i=0; i<this.productionPlan.id.length; i++) {
-      for (let e=0; e<this.workspaces.id.length; e++) {
-        if (this.workspaces.id[e] === this.productionPlan.id[i]) {
-          this.workspacePlan.totalTime[this.workspaces.workspace[e]] += this.productionPlan.amount[i]*this.workspaces.time[e];
+    this.previousPeriodData.subscribe(_=> {
+      _.waitinglistworkstations[0].workplace.forEach(workplace=>{
+        if (workplace.waitinglist) {
+          this.productionQue[workplace.item.id] = Number(workplace.item.timeneed);
         }
-      }
+      });
+      _.ordersinwork[0].workplace.forEach(workplace=>{
+          this.inProduction[workplace.item.id] = Number(workplace.item.timeneed);
+      });
+      this.calculateWorkspacePlan();
+      console.log(this.setuptime);
 
-    }
-    for (let i=0; i<this.productionQue.workspace.length; i++) {
-      this.workspacePlan.totalTime[this.productionQue.workspace[i]] += this.productionQue.time[i];
-    }
-
-    for (let i=0; i<this.inProduction.workspace.length; i++) {
-      this.workspacePlan.totalTime[this.inProduction.workspace[i]] += this.inProduction.time[i];
-    }
-
-    for (let i=0; i<this.workspacePlan.totalTime.length; i++) {
-      if (this.workspacePlan.totalTime[i]<=3600) {
-        this.workspacePlan.shift[i] = 1;
-      } else if (this.workspacePlan.totalTime[i]>3600 && this.workspacePlan.totalTime[i]<=6000) {
-        this.workspacePlan.shift[i] = 2;
-      } else if (this.workspacePlan.totalTime[i]>6000 && this.workspacePlan.totalTime[i]<=7200) {
-        this.workspacePlan.shift[i] = 3;
-      }
-
-      if ((this.workspacePlan.totalTime[i]- (2400*this.workspacePlan.shift[i])>0)) {
-        this.workspacePlan.overtime[i] = (this.workspacePlan.totalTime[i] - (2400 * this.workspacePlan.shift[i])) / 5
-      }
-
-    }
-    console.log(this.workspacePlan);
+    });
 
   }
+
+  calculateWorkspacePlan() {
+    for (let i=0; i<this.productionPlan.item.length; i++) {
+      for (let e=0; e<this.worktime.item.length; e++) {
+        if (this.worktime.item[e] === this.productionPlan.item[i]) {
+          this.workplacePlan.totalTime[this.worktime.workplace[e]] += this.productionPlan.amount[i]*this.worktime.time[e];
+
+
+        }
+      }
+    }
+
+    for (let i=0; i<this.productionQue.length; i++) {
+      this.workplacePlan.totalTime[i] += this.productionQue[i];
+    }
+
+    for (let i=0; i<this.inProduction.length; i++) {
+      this.workplacePlan.totalTime[i] += this.inProduction[i];
+    }
+
+
+    for (let i=0; i<this.workplacePlan.totalTime.length; i++) {
+      if (this.workplacePlan.totalTime[i]<=3600) {
+        this.workplacePlan.shift[i] = 1;
+      } else if (this.workplacePlan.totalTime[i]>3600 && this.workplacePlan.totalTime[i]<=6000) {
+        this.workplacePlan.shift[i] = 2;
+      } else if (this.workplacePlan.totalTime[i]>6000 && this.workplacePlan.totalTime[i]<=7200) {
+        this.workplacePlan.shift[i] = 3;
+      }
+
+      if ((this.workplacePlan.totalTime[i]- (2400*this.workplacePlan.shift[i])>0)) {
+        this.workplacePlan.overtime[i] = (this.workplacePlan.totalTime[i] - (2400 * this.workplacePlan.shift[i])) / 5
+      }
+
+    }
+
+  }
+
 
 }
