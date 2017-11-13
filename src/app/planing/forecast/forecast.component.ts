ef9ef0be2aefa062d0bad822d658dfd68f0853ee
FeWarth <felix.warth@gmx.de>
2017-11-13 17:57:05 +0100

    removed unused code

--- src/app/planing/forecast/forecast.component.ts
+++ src/app/planing/forecast/forecast.component.ts
@@ -13,8 +13,6 @@ export class ForecastComponent implements OnInit {
   sumThird: number;
   sumFourth: number;
   curPeriod = 3;
-  displayedColumns = ['empty', 'Auftraege', 'GeplanteAuftraege1', 'GeplanteAuftraege2', 'GeplanteAuftraege3'];
-  //dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
 
   constructor() { 
     this.forecastPeriod = [
@@ -58,23 +56,3 @@ export class ForecastComponent implements OnInit {
     });
   }
 }
-
-export interface Element {
-  empty: string;
-  Auftraege: number;
-  GeplanteAuftraege1: number;
-  GeplanteAuftraege2: number;
-  GeplanteAuftraege3: number;
-}
-
-/*
-
-const ELEMENT_DATA: Element[] = [
-  {empty: 'Periode', Auftraege: 1, GeplanteAuftraege1: 2, GeplanteAuftraege2: 3, GeplanteAuftraege3: 4},
-  {empty: 'P1', Auftraege: 100, GeplanteAuftraege1: 50, GeplanteAuftraege2: 75, GeplanteAuftraege3: 100},
-  {empty: 'P2', Auftraege: 100, GeplanteAuftraege1: 50, GeplanteAuftraege2: 75, GeplanteAuftraege3: 100},
-  {empty: 'P3', Auftraege: 100, GeplanteAuftraege1: 50, GeplanteAuftraege2: 75, GeplanteAuftraege3: 100},
-  {empty: 'Summe', Auftraege: 300, GeplanteAuftraege1: 150, GeplanteAuftraege2: 225, GeplanteAuftraege3: 300},
-];
-
-*/
