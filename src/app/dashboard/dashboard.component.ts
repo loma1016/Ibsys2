a47566562a9f952e8467e877779ba62d6145c3eb
FeWarth <felix.warth@gmx.de>
2017-11-14 12:24:10 +0100

    add profitchart to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -47,6 +47,30 @@ export class DashboardComponent implements OnInit {
     maintainAspectRatio: false,
   };
 
+  public allProfits: Array<any> = [];  
+  public cumProfits: Array<any> = [];  
+  public summaryChartData: any[] = [{ data: [] }, { data: [] }];
+  public profitChartLabels: Array<any> = [];
+  public profitChartOptions = {
+    responsive: false,
+    maintainAspectRatio: false
+  };
+
+  public profitChartColors:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(7, 137, 232, 0.2)',
+      borderColor: 'rgba(0, 147, 255, 0.7)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.0)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)'
+      },
+  ]
 
   constructor(private db: AngularFireDatabase) {
   }
@@ -79,9 +103,24 @@ export class DashboardComponent implements OnInit {
       ]
     }
 
+    var drawProfitSummary = function(periods) {
+      for (var period in periods) {
+        if (period) {
+          context.allProfits.push(Number(periods[period].result[0].summary[0].profit[0].item.current));
+          context.cumProfits.push(Number(periods[period].result[0].summary[0].profit[0].item.all))
+        }
+      }
+
+      context.summaryChartData = [
+        {data: context.cumProfits, label: 'Kumulierter Profit'},       
+        {data: context.allProfits, label: 'Profit'}
+      ] 
+    }
+
     this.db.object('periods').valueChanges().subscribe(periods => {
       drawTotalStockValue(periods);
       drawAllStockValues(periods);
+      drawProfitSummary(periods);
     });
   }
 }
