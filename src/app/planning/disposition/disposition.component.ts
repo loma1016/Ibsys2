6a2a421e83c091709b0e40ff79476fb9d6bacd21
Jona Wössner <jonawoessner@gmail.com>
2017-11-15 18:12:30 +0100

    first finished version of disposition

--- src/app/planning/disposition/disposition.component.ts
+++ src/app/planning/disposition/disposition.component.ts
@@ -24,13 +24,20 @@ export class DispositionComponent implements OnInit {
 
   ngOnInit() {
     this.db.object('currentPeriod').valueChanges().subscribe(currentPeriod => {
-      this.previousPeriodData = this.db.object('periods/' + (Number(currentPeriod)-1).toString()).valueChanges();
+      this.currentPeriod = Number(currentPeriod);
+      this.previousPeriodData = this.db.object('periods/' + (this.currentPeriod-1).toString()).valueChanges();
       this.previousPeriodData.subscribe(_=> {
-        console.log(_);
 
         _.warehousestock[0].article.forEach( article => {
           if (this.ordersData.index.indexOf(Number(article.item.id)) >= 0) {
-            this.ordersData[article.item.id].inStock = article.item.amount;
+            this.ordersData[article.item.id].inStock = Number(article.item.amount);
+          }
+        });
+        _.futureinwardstockmovement[0].order.forEach( item => {
+          if (this.ordersData.index.indexOf(Number(item.item.article)) >=0 ) {
+            this.ordersData[item.item.article].futureInwardStockMovement.amount = Number(item.item.amount);
+            this.ordersData[item.item.article].futureInwardStockMovement.orderPeriod = Number(item.item.orderperiod);
+            this.ordersData[item.item.article].futureInwardStockMovement.mode = Number(item.item.mode);
           }
         });
         this.forecastData = this.db.object('result/forecast').valueChanges();
@@ -39,10 +46,6 @@ export class DispositionComponent implements OnInit {
           this.calculateDisposition()
 
         });
-
-
-
-        console.log(this.ordersData);
 
       });
 
@@ -57,52 +60,69 @@ export class DispositionComponent implements OnInit {
     let p4 = this.forcast[3].inputs;
 
     this.ordersData.index.forEach(index => {
-      let startAmount = this.ordersData[index].inStock;
+      let orderData = this.ordersData[index];
+      let startAmount = orderData.inStock;
+      let stockMovement = null;
+      if (orderData.futureInwardStockMovement.amount) {
+        stockMovement = orderData.futureInwardStockMovement;
+      }
 
+      for (let i = 0; i <40; i++) {
 
-      console.log(startAmount);
+        if (stockMovement && stockMovement.mode === 5 && (orderData.deliveryTime.normal-((this.currentPeriod-stockMovement.orderPeriod)*5)) === i) {
+          startAmount += stockMovement.amount;
+        } else if (stockMovement && stockMovement.mode === 4 && (orderData.deliveryTime.express-((this.currentPeriod-stockMovement.orderPeriod)*5)) === i) {
+          startAmount += stockMovement.amount;
+        }
 
-
-      for (let i = 0; i <20; i++) {
         if ( i <5) {
-          startAmount -= (this.ordersData[index].usedIn.p1* p1.P1*0.2)+(this.ordersData[index].usedIn.p2* p1.P2*0.2)+(this.ordersData[index].usedIn.p3* p1.P3*0.2);
+          startAmount -= (orderData.usedIn.p1* Number(p1.P1)*0.2)+(orderData.usedIn.p2* Number(p1.P2)*0.2)+(orderData.usedIn.p3* Number(p1.P3)*0.2);
 
         } else if (i <10 ) {
-          startAmount -= (this.ordersData[index].usedIn.p1* p2.P1*0.2)+(this.ordersData[index].usedIn.p2* p2.P2*0.2)+(this.ordersData[index].usedIn.p3* p2.P3*0.2);
+          startAmount -= (orderData.usedIn.p1* Number(p2.P1)*0.2)+(orderData.usedIn.p2* Number(p2.P2)*0.2)+(orderData.usedIn.p3* Number(p2.P3)*0.2);
 
         } else if (i <15) {
-          startAmount -= (this.ordersData[index].usedIn.p1* p3.P1*0.2)+(this.ordersData[index].usedIn.p2* p3.P2*0.2)+(this.ordersData[index].usedIn.p3* p3.P3*0.2);
+          startAmount -= (orderData.usedIn.p1* Number(p3.P1)*0.2)+(orderData.usedIn.p2* Number(p3.P2)*0.2)+(orderData.usedIn.p3* Number(p3.P3)*0.2);
 
         } else if (i <20) {
-          startAmount -= (this.ordersData[index].usedIn.p1* p4.P1*0.2)+(this.ordersData[index].usedIn.p2* p4.P2*0.2)+(this.ordersData[index].usedIn.p3* p4.P3*0.2);
+          startAmount -= (orderData.usedIn.p1* Number(p4.P1)*0.2)+(orderData.usedIn.p2* Number(p4.P2)*0.2)+(orderData.usedIn.p3* Number(p4.P3)*0.2);
 
+        } else {
+          startAmount -= (orderData.usedIn.p1* ((Number(p1.P1)+Number(p2.P1)+Number(p3.P1)+Number(p4.P1))/4)/5)+
+                         (orderData.usedIn.p2* ((Number(p1.P1)+Number(p2.P1)+Number(p3.P1)+Number(p4.P1))/4)/5)+
+                         (orderData.usedIn.p3* ((Number(p1.P1)+Number(p2.P1)+Number(p3.P1)+Number(p4.P1))/4)/5);
         }
 
         if (startAmount < 0) {
-          if (this.ordersData[index].diliveryTime.normal+5 < i) {
-            console.log("next period n");
+          if (orderData.deliveryTime.normal+5 < i) {
             break
-          } else if (this.ordersData[index].diliveryTime.normal < i) {
-            this.ordersData[index].result.normalOrder = this.ordersData[index].discontAmount;
-            console.log("this period n");
+          } else if (orderData.deliveryTime.normal < i) {
+            this.ordersData[index].result.normalOrder = orderData.discontAmount;
             break
-          } else if (this.ordersData[index].diliveryTime.express < i) {
-            this.ordersData[index].result.expressOrder = this.ordersData[index].discontAmount;
-            console.log("this period e");
+          } else if (orderData.deliveryTime.express < i) {
+            this.ordersData[index].result.expressOrder = orderData.discontAmount;
             break
           } else {
-            this.ordersData[index].result.expressOrder = this.ordersData[index].discontAmount;
-            console.log("also this period e");
+            this.ordersData[index].result.expressOrder = orderData.discontAmount;
             break
           }
         }
-        console.log(i);
-        console.log(startAmount);
 
       }
 
-      console.log('_____________________')
     });
+
+    let result = {};
+
+    this.ordersData.index.forEach(index => {
+      if (this.ordersData[index].result.normalOrder || this.ordersData[index].result.expressOrder ) {
+        result[index] = {normalOrder: this.ordersData[index].result.normalOrder, expressOrder: this.ordersData[index].result.expressOrder }
+      }
+    });
+
+    this.db.object('/result/disposition').update(result);
+
+
   }
 }
 
