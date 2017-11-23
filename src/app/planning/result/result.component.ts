77bce78695ac5a45a1b382e24eeb7ece6e8fac5f
Jona Wössner <jonawoessner@gmail.com>
2017-11-23 17:15:33 +0100

    add calculation of sales, workplace cost and maschine cost to result

--- src/app/planning/result/result.component.ts
+++ src/app/planning/result/result.component.ts
@@ -18,42 +18,90 @@ export class ResultComponent implements OnInit {
     workingtimelist: {workingtime:[]},
   };
 
+  sales = 0;
+
+  costs = {
+    shifts: 0,
+    machine: 0
+  };
+
+  costsData = {
+    shifts : {
+      first: 0.45,
+      second: 0.55,
+      third: 0.70,
+      overtime: 0.90
+    },
+    machine: [0.06,0.06,0.06,0.06,0,0.4,0.4,0.4,1.05,0.4,0.4,0.4,0.65,0.06,0.06]
+  };
+
   constructor(private db: AngularFireDatabase) {}
 
   ngOnInit() {
     this.db.object('result').valueChanges().subscribe(result=> {
-      this.resetResult();
 
-      this.result.sellwish.item = [
-        {"@": {article:1, quantity:Number((result as any).forecast[0].inputs.P1)}},
-        {"@": {article:2, quantity: Number((result as any).forecast[0].inputs.P2)}},
-        {"@": {article:3, quantity: Number((result as any).forecast[0].inputs.P3)}}
-      ];
-      this.result.selldirect.item = [
-        {"@": {article:1, quantity:0, price:0, penalty:0}},
-        {"@": {article:2, quantity:0, price:0, penalty:0}},
-        {"@": {article:2, quantity:0, price:0, penalty:0}}
-      ];
+      this.setResult(result);
 
-      if ((result as any).production) {
-        (result as any).production.item.forEach((item, index) => {
-          this.result.productionlist.production.push({"@": {article:item, quantity: (result as any).production.amount[index]}})
-        });
-      }
+      console.log(result);
 
-      if ((result as any).workingtimelist) {
-        (result as any).workingtimelist.forEach(place => {
-          this.result.workingtimelist.workingtime.push({"@": {station: place.station, schift: place.shift, overtime: place.overtime}})
-        });
-      }
+      this.sales = 200* (Number((result as any).forecast[0].inputs.P1) + Number((result as any).forecast[0].inputs.P2) + Number((result as any).forecast[0].inputs.P3));
 
-     if ((result as any).disposition) {
-        (result as any).disposition.forEach(order => {
-          this.result.orderlist.order.push({"@": {article:order.article, quantity: order.quantity, modus: order.modus}})
-        });
-      }
+      let wageCost = 0;
+      let machineCost = 0;
+      (result as any).workingtimelist.forEach((place,index) => {
+        if (place.shift === 1) {
+          wageCost += 2400*this.costsData.shifts.first + place.overtime*5*this.costsData.shifts.overtime;
 
+        } else if (place.shift === 2) {
+          wageCost += 2400*this.costsData.shifts.first + 2400*this.costsData.shifts.second + place.overtime*5*this.costsData.shifts.overtime;
+        } else if (place.shift === 3) {
+          wageCost += 2400*this.costsData.shifts.first + 2400*this.costsData.shifts.second +  2400*this.costsData.shifts.third;
+        }
+
+        machineCost += (2400 * place.shift + place.overtime*5)*this.costsData.machine[index];
+
+
+
+        this.costs.machine = Math.round(machineCost);
+        this.costs.shifts = Math.round(wageCost);
+      });
+
+      console.log(this.costs);
     });
+  }
+
+  setResult(result: any) {
+
+    this.resetResult();
+
+    this.result.sellwish.item = [
+      {"@": {article:1, quantity: Number(result.forecast[0].inputs.P1)}},
+      {"@": {article:2, quantity: Number(result.forecast[0].inputs.P2)}},
+      {"@": {article:3, quantity: Number(result.forecast[0].inputs.P3)}}
+    ];
+    this.result.selldirect.item = [
+      {"@": {article:1, quantity:0, price:0, penalty:0}},
+      {"@": {article:2, quantity:0, price:0, penalty:0}},
+      {"@": {article:2, quantity:0, price:0, penalty:0}}
+    ];
+
+    if (result.production) {
+      result.production.item.forEach((item, index) => {
+        this.result.productionlist.production.push({"@": {article:item, quantity: result.production.amount[index]}})
+      });
+    }
+
+    if (result.workingtimelist) {
+      result.workingtimelist.forEach(place => {
+        this.result.workingtimelist.workingtime.push({"@": {station: place.station, schift: place.shift, overtime: place.overtime}})
+      });
+    }
+
+    if (result.disposition) {
+      result.disposition.forEach(order => {
+        this.result.orderlist.order.push({"@": {article:order.article, quantity: order.quantity, modus: order.modus}})
+      });
+    }
   }
 
   resetResult(){
@@ -66,7 +114,6 @@ export class ResultComponent implements OnInit {
       workingtimelist: {workingtime:[]},
     };
   }
-
 
   exportToXml() {
     var js2xmlparser = require("js2xmlparser");
