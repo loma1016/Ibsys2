754b78e096231432978ea1213bcddb77e1f50bb9
Daniel Weber <wedaniel1992@gmail.com>
2017-11-27 13:15:04 +0100

    Added environment.test.ts for testing

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBYHBFYvYn7bbhPSTUQG0oQCyNWJGvh0c4',
+    authDomain: 'planningtool-test.firebaseapp.com',
+    databaseURL: 'https://planningtool-test.firebaseio.com',
+    projectId: 'planningtool-test',
+    storageBucket: '',
+    messagingSenderId: '230306959721'
+  }
+};
