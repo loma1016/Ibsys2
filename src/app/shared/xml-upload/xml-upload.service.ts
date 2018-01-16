3eb543d2d579f9a2cee2dd0c71eebe948c518760
FeWarth <felix.warth@gmx.de>
2018-01-16 12:26:01 +0100

    remove senseless group restriction for XML Upload

--- src/app/shared/xml-upload/xml-upload.service.ts
+++ src/app/shared/xml-upload/xml-upload.service.ts
@@ -5,16 +5,14 @@ import { ToastyServiceInt } from "../../util/toasty.service";
 
 @Injectable()
 export class XmlUploadService {
-    group = 11;
-
+    
     constructor(private db: AngularFireDatabase, private toastyService: ToastyServiceInt, private toastyConfig: ToastyConfig) {
         this.toastyConfig.theme = 'material';
     }
 
     uploadPeriod(val, setPeriod): any {
         var period = val.$.period;
-        var group = val.$.group;
-        if (period && group == this.group) {
+        if (period) {
             delete val.$;
             var jsonStr = JSON.stringify(val);
             var jsonStrRep = jsonStr.replace(/\$/g , 'item');
