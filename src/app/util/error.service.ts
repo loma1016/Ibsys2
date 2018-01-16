4fca96b6f77dcc7999c49552bd8defb1729ab290
Jona Wössner <jonawoessner@gmail.com>
2018-01-16 15:41:43 +0100

    adderror log

--- src/app/util/error.service.ts
+++ src/app/util/error.service.ts
@@ -3,5 +3,7 @@ import { ErrorHandler } from '@angular/core';
 export default class MyErrorHandler implements ErrorHandler {
   handleError(error) {
     //window.location.reload();
+
+    console.log(error);
   }
 }
